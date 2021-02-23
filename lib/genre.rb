0c053cc647dbeed44adb29204ed7f3e977ccc4fb
yves <yvesmomplais@hotmail.com>
2021-02-22 22:41:11 -0500

    commit tree e23f51aefa63ac707464d4303a66980b0ef1697f

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,22 @@
+class Genre
+    attr_accessor :name
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all { |song| song.genre == self}
+    end
+
+    def artists
+        songs.map(&:artist)
+    end
+end        
